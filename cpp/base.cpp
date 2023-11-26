1b3f9de34f357b54d821d9b1dd667a40ced818b1
luuguas <69027878+luuguas@users.noreply.github.com>
2023-11-26 13:11:51 +0000

    Rename base.cpp / Add pre.cpp

--- cpp/base.cpp
+++ cpp/base.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <filesystem>
 
 #include <vector>
 #include <algorithm>
---
+++ cpp/pre.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <random>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    long long N, L, R;
+    cin >> N >> L >> R;
+    
+    vector<long long> array(N);
+
+    //ランダムなシード値を使って乱数生成器を初期化
+    random_device rd;
+    mt19937_64 mt(rd());
+
+    //生成する乱数の最小値L,最大値Rを指定して初期化
+    uniform_int_distribution<long long> uid(L, R);
+
+    for(long long i = 0; i < N; ++i)
+    {
+        //L以上R以下のランダムな整数を等確率で生成
+        array[i] = uid(mt);
+    }
+
+    //乱数列を出力
+    for(long long i = 0; i < N; ++i)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
